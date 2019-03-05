44799dd29e1cf7c0e8c70badc2a817c655058148
Richard Lister <rlister@gmail.com>
2019-03-05 11:03:31 -0500

    add my classtitle patch

--- dwm.c
+++ dwm.c
@@ -2008,8 +2008,8 @@ updatestatus(void)
 void
 updatetitle(Client *c)
 {
-	if (!gettextprop(c->win, netatom[NetWMName], c->name, sizeof c->name))
-		gettextprop(c->win, XA_WM_NAME, c->name, sizeof c->name);
+        /* rjl: use window class instead of name */
+        gettextprop(c->win, XA_WM_CLASS, c->name, sizeof c->name);
 	if (c->name[0] == '\0') /* hack to mark broken clients */
 		strcpy(c->name, broken);
 }
