fe11f75f1411c73d9d150501848f9c07be093375
Richard Lister <rlister@gmail.com>
2019-03-04 23:43:22 -0500

    green and black colour scheme

--- config.h
+++ config.h
@@ -12,10 +12,14 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_black[]       = "#000000";
+static const char col_green[]       = "#98fb98"; /* PaleGreen */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/* [SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, */
+	/* [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, */
+	[SchemeNorm] = { col_gray3, col_black, col_black },
+	[SchemeSel]  = { col_green, col_black, col_gray2 },
 };
 
 /* tagging */
