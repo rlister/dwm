bf3a547e2c23f76cf4970718a1c4dffbfffe6a29
Richard Lister <rlister@gmail.com>
2019-03-05 10:11:36 -0500

    fix typo

--- config.h
+++ config.h
@@ -51,8 +51,8 @@ static const Layout layouts[] = {
 
 /* key definitions */
 /* #define MODKEY Mod1Mask */
-+#define MODKEY Mod4Mask         /* super */
-+#define META Mod1Mask
+#define MODKEY Mod4Mask         /* super */
+#define META Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
