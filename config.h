095ad92d8c5041549dcf814415a99d0c09e135e3
Richard Lister <rlister@gmail.com>
2019-04-02 13:54:20 -0400

    passmenu

--- config.h
+++ config.h
@@ -69,10 +69,11 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "combi" };
+static const char *passmenu[]  = { "/usr/share/doc/pass/examples/dmenu/passmenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	/* { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, */
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -116,6 +117,7 @@ static Key keys[] = {
 	{ META,                         XK_F10,    setlayout,      {.v = &layouts[1]} },
 	{ META,                         XK_F11,    setlayout,      {.v = &layouts[2]} },
         { META,                         XK_F12,    setlayout,      {.v = &layouts[3]} },
+        { MODKEY,                       XK_p,      spawn,          {.v = passmenu } },
 };
 
 /* button definitions */
