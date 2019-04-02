ed177f87acbda3efed72ed8e43f5f672c672cae5
Richard Lister <rlister@gmail.com>
2019-04-02 14:01:27 -0400

    panel experiments

--- config.h
+++ config.h
@@ -32,7 +32,11 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
+	{ "xfce4-panel", NULL,    NULL,       ~0,	    True,	 -1 },
+	{ "Xfce4-panel", NULL,    NULL,       ~0,	    True,	 -1 },
+	{ "panel",    NULL,       NULL,       ~0,	    True,	 -1 },
+        { NULL,          NULL,    "xfce4-panel", ~0,        True,        -1 },
+        /* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 };
 
 /* layout(s) */
