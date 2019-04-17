3f000b8723c253defc31a5252ae4b750705f0eb8
Richard Lister <rlister@gmail.com>
2019-04-17 10:14:08 -0400

    arch pass pkg installs passmenu in /usr/bin, so let PATH find it

--- config.h
+++ config.h
@@ -74,7 +74,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "combi" };
-static const char *passmenu[]  = { "/usr/share/doc/pass/examples/dmenu/passmenu", NULL };
+static const char *passmenu[]  = { "passmenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
