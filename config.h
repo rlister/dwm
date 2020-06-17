fd6049e0d42b03213f29aeb029f90ed6631844f6
Richard Lister <rlister@gmail.com>
2020-06-17 17:01:21 -0400

    new passcmd using rofi

--- config.h
+++ config.h
@@ -74,7 +74,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "combi" };
-static const char *passmenu[]  = { "passmenu", NULL };
+static const char *passcmd[] = { "passmenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -120,7 +120,7 @@ static Key keys[] = {
         /* my extras */
         { MODKEY,                       XK_space,  spawn,          {.v = roficmd } },
 	{ MODKEY|ControlMask,           XK_space,  setlayout,      {0} },
-        { MODKEY,                       XK_p,      spawn,          {.v = passmenu } },
+        { MODKEY,                       XK_p,      spawn,          {.v = passcmd } },
 };
 
 /* button definitions */
