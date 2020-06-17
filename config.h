9f722be0dd55bbfccd5cb2c37cbb8433b0984a56
Richard Lister <rlister@gmail.com>
2020-06-17 17:02:25 -0400

    missing null terminator

--- config.h
+++ config.h
@@ -72,8 +72,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *roficmd[]  = { "rofi", "-show", "combi" };
 static const char *termcmd[] = { "termite", NULL };
+static const char *roficmd[] = { "rofi", "-show", "combi", NULL };
 static const char *passcmd[] = { "passmenu", NULL };
 
 static Key keys[] = {
