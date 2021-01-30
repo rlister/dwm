f66a585d0bab4d0811d518f74380ee7c037cf0bf
Richard Lister <rlister@gmail.com>
2021-01-30 15:15:34 -0500

    add tcl layout

--- config.h
+++ config.h
@@ -36,11 +36,13 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+#include "tcl.c"
 static const Layout layouts[] = {
 	/* symbol               function */
 	{ "[T]", tile }, /* first entry is default */
 	{ "[F]", NULL }, /* no layout function means floating behavior */
 	{ "[M]", monocle },
+	{ "[C]", tcl },
 };
 
 /* key definitions */
@@ -76,6 +78,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
